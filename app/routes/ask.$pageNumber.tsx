2050bf3a54b9f64db33ddf8c310309a670a7172b
Shantanu Dutta <connect.shantanu.dutta@gmail.com>
2022-01-14 20:28:22 +0530

    added `ask` route

---
+++ app/routes/ask.$pageNumber.tsx
@@ -0,0 +1,48 @@
+import { Link, useLoaderData } from "remix";
+import type { LoaderFunction } from "remix";
+
+import type { ArticleResponseType, ArticleType } from "~/types";
+
+import ArticleList from "~/components/ArticleList";
+
+type LoaderData = {
+  results: ArticleType[];
+  pageNumber: number;
+};
+
+export const loader: LoaderFunction = async ({ params }) => {
+  const pageNumber = parseInt(params.pageNumber || "1");
+  const res = await fetch(`https://api.hnpwa.com/v0/ask/${pageNumber}.json`);
+  const results = (await res.json()) as ArticleResponseType[];
+  const resultsView = results.map(
+    (
+      { id, url, title, domain, points, user, time_ago, comments_count },
+      index
+    ) => {
+      return {
+        id,
+        url,
+        title,
+        domain,
+        points,
+        user,
+        time_ago,
+        comments_count,
+        sequenceNumber: (pageNumber - 1) * 30 + index + 1,
+      } as ArticleType;
+    }
+  );
+  return { results: resultsView, pageNumber } as LoaderData;
+};
+
+export default function AskParamRoute() {
+  const data = useLoaderData<LoaderData>();
+  return (
+    <section>
+      <ArticleList articles={data.results} />
+      <div className="more">
+        <Link to={`/new/${data.pageNumber + 1}`}>More...</Link>
+      </div>
+    </section>
+  );
+}
